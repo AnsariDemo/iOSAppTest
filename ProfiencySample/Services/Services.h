2f464bba1abb4065104bce05eb15f4b4156420c5
rajesh57 <rajeshinvotech@gamail.com>
2015-11-19 23:03:07 +0530

    first commit

---
+++ ProfiencySample/Services/Services.h
@@ -0,0 +1,15 @@
+//
+//  Services.h
+//  ProfiencySample
+//
+//  Created by Cognizant on 11/19/15.
+//  Copyright (c) 2015 Ansari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Services : NSObject
+
+typedef void (^CompletionBlock)(NSDictionary *response, NSError *error);
++(void)getFeedResponseForUrl:(NSString *)url withCallback:(CompletionBlock)callback;
+@end
